65263bad1bc68bdbe81e15c4d46c1e39c34b0c6b
varnerlab <jdv27@cornell.edu>
2018-06-21 08:41:17 -0400

    Initial Commit

---
+++ VLModelKitCore/VLModelKitCore.h
@@ -0,0 +1,19 @@
+//
+//  VLModelKitCore.h
+//  VLModelKitCore
+//
+//  Created by Jeffrey Varner on 6/21/18.
+//  Copyright © 2018 Varnerlab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for VLModelKitCore.
+FOUNDATION_EXPORT double VLModelKitCoreVersionNumber;
+
+//! Project version string for VLModelKitCore.
+FOUNDATION_EXPORT const unsigned char VLModelKitCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VLModelKitCore/PublicHeader.h>
+
+
